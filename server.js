a01ade6e3f7548222d29450b61c0f141c328666e
Pere Grinus <gregjleighton@gmail.com>
2021-01-05 16:33:16 -0500

    set up server JS boilerplate to get express running

---
+++ server.js
@@ -0,0 +1,21 @@
+// Set up dependencies
+var express = require("express");
+
+// create express server
+var app = express();
+
+// set listening PORT
+var PORT = process.env.PORT || 8080;
+
+// set up express to handle note data and parse 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// need to setup ROUTER to get to note files / db folder
+
+
+// Listener to start the server
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT)
+});
